3e2fc3248aabc73e31bd7a8f578b606800c401c9
D3m0n4U <160055791+D3m0n4U@users.noreply.github.com>
2024-06-27 02:50:09 -0700

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,2 +1,3 @@
-echo "Paaji Challenge ni krne ka....hehe...aaj ki party aaki trf se" >> "%temp%\..\..\Desktop\ksi rhi.txt"
-start notepad "%temp%\..\..\Desktop\ksi rhi.txt"
+:: echo "Paaji Challenge ni krne ka....hehe...aaj ki party aaki trf se" >> "%temp%\..\..\Desktop\ksi rhi.txt"
+:: start notepad "%temp%\..\..\Desktop\ksi rhi.txt"
+start msedge https://www.youtube.com
