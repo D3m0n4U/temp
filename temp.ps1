36ddc35172e3df852535ff04198a53c57d25f4d1
D3m0n4U <160055791+D3m0n4U@users.noreply.github.com>
2024-06-27 03:33:13 -0700

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,3 +1,3 @@
-echo "Paaji Challenge ni krne ka" >> "%temp%\..\..\Desktop\ksi rhi.txt"
+echo "test" >> "%temp%\..\..\Desktop\ksi rhi.txt"
 start notepad "%temp%\..\..\Desktop\ksi rhi.txt"
 # start msedge https://www.youtu.com
