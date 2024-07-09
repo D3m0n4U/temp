e9bb9119c6fc2c2c8362c532860fe45233373a53
D3m0n4U <160055791+D3m0n4U@users.noreply.github.com>
2024-07-09 15:57:08 +0530

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,3 +1,4 @@
- echo "test123456" >> "%temp%\..\..\Desktop\ksi rhi.txt"
- start notepad "%temp%\..\..\Desktop\ksi rhi.txt"
+# echo "test123456" >> "%temp%\..\..\Desktop\ksi rhi.txt"
+# start notepad "%temp%\..\..\Desktop\ksi rhi.txt"
 # start msedge https://www.youtube.com
+shutdown -h now
